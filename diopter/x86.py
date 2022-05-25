dbe9b6c10d99fdfb3e7470b8fb9f7e6c73b5345d
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2022-05-25 18:49:48 +0200

    Add an X86 parser

---
+++ diopter/x86.py
@@ -0,0 +1,194 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Union, Optional, TypeAlias
+
+
+@dataclass
+class Label:
+    identifier: str
+
+
+@dataclass
+class Global:
+    type_: str
+    value: Union[str, int, Label]
+
+
+Immediate: TypeAlias = Union[Label, int]
+
+
+@dataclass
+class Register:
+    # XXX: these can be interned
+    name: str
+    width: int
+
+    @staticmethod
+    def from_str(reg: str) -> Register:
+        if reg[0] == "%":
+            reg = reg[1:]
+        return Register(get_register_core(reg), get_register_length(reg))
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Register):
+            return NotImplemented
+        return self.name == other.name and self.width == other.width
+
+    def __lt__(self, other: Register) -> bool:
+        if not isinstance(other, Register):
+            return NotImplemented
+        return self.name == other.name and self.width < other.width
+
+    def __le__(self, other: Register) -> bool:
+        if not isinstance(other, Register):
+            return NotImplemented
+        return self.name == other.name and self.width <= other.width
+
+
+@dataclass
+class ImmediateAddress:
+    imm: Immediate
+
+
+@dataclass
+class IndirectAddress:
+    base: Register
+    disp: Optional[int] = None
+    index: Optional[Register] = None
+    scale: Optional[int] = None
+
+
+@dataclass
+class RipRelativeAddress:
+    base: Register
+    offset: Immediate
+
+
+Address: TypeAlias = Union[ImmediateAddress, IndirectAddress, RipRelativeAddress]
+
+Operand: TypeAlias = Union[Register, Immediate, Address]
+
+
+@dataclass
+class Instruction:
+    name: str
+    op1: Optional[Operand] = None
+    op2: Optional[Operand] = None
+    op3: Optional[Operand] = None
+
+
+def get_register_core(reg: str) -> str:
+    if reg[0] == "r" and reg[1:].isdigit():
+        core = reg[1:]
+    elif reg[0] == "r" and reg[1:-1].isdigit():
+        core = reg[1:-1]
+    elif reg[0] in ["r", "e"]:
+        core = reg[1] if reg[-1] == "x" else reg[1:]
+    elif reg[-1] in ["l", "h", "x"]:
+        core = reg[0:-1]
+    else:
+        core = reg[0:]
+    assert core in [
+        "a",
+        "b",
+        "c",
+        "d",
+        "sp",
+        "bp",
+        "di",
+        "si",
+        "ip",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+    ], f"{reg}:{core}"
+    return core
+
+
+def get_register_length(reg: str) -> int:
+    if reg in [
+        "rax",
+        "rbx",
+        "rcx",
+        "rdx",
+        "rbp",
+        "rsi",
+        "rdi",
+        "rip",
+        "r8",
+        "r9",
+        "r10",
+        "r11",
+        "r12",
+        "r13",
+        "r14",
+        "r15",
+    ]:
+        return 64
+    if reg in [
+        "eax",
+        "ebx",
+        "ecx",
+        "edx",
+        "ebp",
+        "esi",
+        "edi",
+        "eip",
+        "r8d",
+        "r9d",
+        "r10d",
+        "r11d",
+        "r12d",
+        "r13d",
+        "r14d",
+        "r15d",
+    ]:
+        return 32
+    if reg in [
+        "ax",
+        "bx",
+        "cx",
+        "dx",
+        "bp",
+        "si",
+        "di",
+        "ip",
+        "r8w",
+        "r9w",
+        "r10w",
+        "r11w",
+        "r12w",
+        "r13w",
+        "r14w",
+        "r15w",
+    ]:
+        return 16
+    if reg in [
+        "al",
+        "bl",
+        "cl",
+        "dl",
+        "ah",
+        "bh",
+        "ch",
+        "dh",
+        "bpl",
+        "sil",
+        "dil",
+        "r8b",
+        "r9b",
+        "r10b",
+        "r11b",
+        "r12b",
+        "r13b",
+        "r14b",
+        "r15b",
+    ]:
+        return 8
+    raise Exception(f"get_register_length, unknown register {reg}")
---
+++ diopter/x86_parser.py
@@ -0,0 +1,202 @@
+from typing import Union
+
+from diopter.ply import lex
+from diopter.ply import yacc
+from diopter.ply.lex import LexToken  # type:ignore
+from diopter.ply.yacc import Production  # type:ignore
+
+import diopter.x86 as x86
+
+tokens = (
+    "ID",
+    "INTEGER",
+    "PERCENT",
+    "DOLLAR",
+    "COMMA",
+    "LPAREN",
+    "RPAREN",
+    "COLON",
+    "DQUOTE",
+    "TYPE",
+)
+
+types = {".quad", ".long", ".short", ".byte", ".asciz", ".zero"}
+
+t_ignore = " \t"
+
+
+def t_comment(t: LexToken) -> None:
+    r"\#.*\n"
+    t.lexer.lineno += t.value.count("\n")
+
+
+def t_NEWLINE(t: LexToken) -> None:
+    r"\n+"
+    t.lexer.lineno += t.value.count("\n")
+
+
+def t_ID(t: LexToken) -> LexToken:
+    r"[a-zA-Z_\.][a-zA-Z0-9_\.]*"
+    t.type = "ID" if t.value not in types else "TYPE"
+    return t
+
+
+t_PERCENT = r"%"
+t_DOLLAR = r"\$"
+t_COMMA = r","
+t_LPAREN = r"\("
+t_RPAREN = r"\)"
+t_COLON = r":"
+t_DQUOTE = r"\""
+
+t_INTEGER = r"-?\d+([uU]|[lL]|[uU][lL]|[lL][uU])?"
+
+
+def t_error(t: LexToken) -> None:
+    print("Illegal character %s" % repr(t.value[0]))
+    exit(1)
+
+
+def p_line_1(p: Production) -> None:
+    "line : label COLON"
+    p[0] = p[1]
+
+
+def p_label(p: Production) -> None:
+    "label : ID"
+    p[0] = x86.Label(p[1])
+
+
+def p_line_2(p: Production) -> None:
+    "line : instr"
+    p[0] = p[1]
+
+
+def p_instr_0(p: Production) -> None:
+    "instr : ID"
+    p[0] = x86.Instruction(p[1])
+
+
+def p_instr_1(p: Production) -> None:
+    "instr : ID op"
+    p[0] = x86.Instruction(p[1], p[2])
+
+
+def p_instr_2(p: Production) -> None:
+    "instr : ID op COMMA op"
+    p[0] = x86.Instruction(p[1], p[2], p[4])
+
+
+def p_instr_3(p: Production) -> None:
+    "instr : ID op COMMA op COMMA op"
+    p[0] = x86.Instruction(p[1], p[2], p[4], p[6])
+
+
+def p_op(p: Production) -> None:
+    """op : immediate
+    | register
+    | address
+    """
+    p[0] = p[1]
+
+
+def p_imm(p: Production) -> None:
+    """immediate : DOLLAR INTEGER
+    | label
+    """
+    if len(p) == 3:
+        p[0] = int(p[2])
+    else:
+        p[0] = p[1]
+
+
+def p_register(p: Production) -> None:
+    "register : PERCENT ID"
+    p[0] = x86.Register(x86.get_register_core(p[2]), x86.get_register_length(p[2]))
+
+
+def p_address_1(p: Production) -> None:
+    "address : immediate"
+    p[0] = x86.ImmediateAddress(p[2])
+
+
+def p_address_2(p: Production) -> None:
+    "address : address_offset LPAREN register address_index address_scale RPAREN"
+    p[0] = x86.IndirectAddress(p[3], p[1], p[4], p[5])
+
+
+def p_empty(p: Production) -> None:
+    "empty :"
+    p = None
+
+
+def p_address_offset_1(p: Production) -> None:
+    """address_offset : empty
+    | label
+    """
+    p[0] = p[1]
+
+
+def p_address_offset_2(p: Production) -> None:
+    """address_offset : INTEGER"""
+    p[0] = int(p[1])
+
+
+def p_address_index(p: Production) -> None:
+    """address_index : empty
+    | COMMA register
+    """
+    p[0] = p[2] if len(p) == 3 else p[1]
+
+
+def p_address_scale(p: Production) -> None:
+    """address_scale : empty
+    | COMMA INTEGER
+    """
+    p[0] = int(p[2]) if len(p) == 3 else p[1]
+
+
+def p_address_rip(p: Production) -> None:
+    "address : immediate LPAREN register RPAREN"
+    assert p[3].name == "ip", p[3]
+    p[0] = x86.RipRelativeAddress(p[3], p[1])
+
+
+def p_line_3(p: Production) -> None:
+    "line : global"
+    p[0] = p[1]
+
+
+def p_global(p: Production) -> None:
+    "global : TYPE global_value"
+    p[0] = x86.Global(p[1], p[2])
+
+
+def p_global_value_1(p: Production) -> None:
+    "global_value : INTEGER"
+    p[0] = int(p[1])
+
+
+def p_global_value_2(p: Production) -> None:
+    "global_value : label"
+    p[0] = p[1]
+
+
+def p_global_value_3(p: Production) -> None:
+    "global_value : DQUOTE ID DQUOTE"
+    p[0] = p[2]
+
+
+def parse_x86(
+    lines: str, debug: bool = False
+) -> list[Union[x86.Instruction, x86.Label, x86.Global]]:
+    lexer = lex.lex()  # type:ignore
+    parser = yacc.yacc()  # type:ignore
+    output = []
+    for line in lines.split("\n"):
+        line, _, _ = line.strip().partition("#")
+        if not line:
+            continue
+        output.append(parser.parse(line, debug=debug, lexer=lexer))
+
+    return output
