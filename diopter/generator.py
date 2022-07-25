1812c93857a118c8147466f22829ef95b35dd9b3
Yann Girsberger <yann@ywg.ch>
2022-07-25 10:55:43 +0200

    [generator] refactor
    New naming convention:
    We first generate raw code which is then filtered based on
    `filter_code`.
    The ParallelGenerator was just one function, so it could be easily moved
    to the Generator.

--- diopter/generator.py
+++ diopter/generator.py
@@ -1,53 +1,42 @@
-import subprocess
 import logging
-from random import randint
-from tempfile import NamedTemporaryFile
+import subprocess
 from abc import ABC, abstractmethod
-from pathlib import Path
-from typing import Optional, Iterator
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from multiprocessing import cpu_count
+from pathlib import Path
+from random import randint
+from tempfile import NamedTemporaryFile
+from typing import Iterator, Optional
 
 from diopter.sanitizer import sanitize_code as sanitize
 
 
 class Generator:
-    def __init__(self, minimum_length: int):
-        self.minimum_length = minimum_length
-
     @abstractmethod
-    def generate_file_impl(self) -> str:
+    def generate_raw_code(self) -> str:
         pass
 
     @abstractmethod
-    def sanitize_code(self, code: str) -> bool:
+    def filter_code(self, code: str) -> bool:
         # TODO: the sanitizer should be configurable, e.g., only warnings + UB
         pass
 
-    def generate_case(self) -> str:
+    def generate_code(self) -> str:
         while True:
-            code = self.generate_file_impl()
-            if self.minimum_length and len(code) < self.minimum_length:
-                continue
-            if self.sanitize_code(code):
+            code = self.generate_raw_code()
+            if self.filter_code(code):
                 return code
-            logging.debug("Code not sanitizable")
+            logging.debug("Code didn't pass the filter")
 
-
-class ParallelGenerator:
-    def __init__(self, generator: Generator):
-        """Args:
-        generator: which Generator to use? E.g., CSmithGenerator
-        """
-        self.generator = generator
-
-    def generate_cases(self, n: int, jobs: Optional[int] = None) -> Iterator[str]:
+    def generate_code_parallel(
+        self, n: int, jobs: Optional[int] = None
+    ) -> Iterator[str]:
         """Args:
         n: how many cases to generate
         jobs: the number of concurrent jobs, if none cpu_count() will be used
         """
         with ProcessPoolExecutor(jobs if jobs else cpu_count()) as p:
-            futures = (p.submit(self.generator.generate_case) for _ in range(n))
+            futures = (p.submit(self.generate_code) for _ in range(n))
             for future in as_completed(futures):
                 yield future.result()
 
@@ -90,18 +79,31 @@ class CSmithGenerator(Generator):
         include_path: Optional[str] = None,
         options_pool: Optional[list[str]] = None,
         minimum_length: int = 10000,
+        maximum_length: int = 50000,
+        clang: str = "clang",
+        gcc: str = "gcc",
+        ccomp: str = "ccomp",
     ):
-        """Args:
-        csmith (str): Path to executable or name in $PATH to csmith, if
-        empty "csmith" will be used
-        include_path (str): The csmith include path, if empty
-        "/usr/include/csmith-2.3.0" will be used
-        options_pool: csmith options that will be randomly selected, if
-        empty default_options_pool will be used
-        minimum_length: the minium length (characters) of a generated test
-        case, smaller ones will be discarded
         """
-        super().__init__(minimum_length)
+        Args:
+            self:
+            csmith (Optional[str]): Path to executable or name in $PATH to csmith, if
+            empty "csmith" will be used
+            include_path (Optional[str]): The csmith include path, if empty
+            "/usr/include/csmith-2.3.0" will be used
+            options_pool (Optional[list[str]]): csmith options that will be randomly selected, if
+            empty default_options_pool will be used
+            minimum_length (int): The minimum length of a generated test case in characters.
+            maximum_length (int): The maximum length of a generated test case in characters.
+            clang (str): Path to executable or name in $PATH to clang. Default: "clang".
+            gcc (str): Path to executable or name in $PATH to gcc. Default: "gcc".
+            ccomp (str): Path to executable or name in $PATH to compcert. Default: "ccomp".
+        """
+        self.minimum_length = minimum_length
+        self.maximum_length = maximum_length
+        self.clang = clang
+        self.gcc = gcc
+        self.ccomp = ccomp
         self.csmith = csmith if csmith else "csmith"
         self.options = (
             options_pool if options_pool else CSmithGenerator.default_options_pool
@@ -110,11 +112,8 @@ class CSmithGenerator(Generator):
             include_path if include_path else "/usr/include/csmith-2.3.0"
         )
 
-    def generate_file_impl(self) -> str:
+    def generate_raw_code(self) -> str:
         """Generate random code with csmith.
-
-        Args:
-            csmith (str): Path to executable or name in $PATH to csmith.
 
         Returns:
             str: csmith generated program.
@@ -130,6 +129,9 @@ class CSmithGenerator(Generator):
         assert result.returncode == 0
         return result.stdout.decode("utf-8")
 
-    def sanitize_code(self, code: str) -> bool:
-        # TODO: don't hand-code the binary names
-        return sanitize("gcc", "clang", "ccomp", code, f"-I {self.include_path}")
+    def filter_code(self, code: str) -> bool:
+        if len(code) < self.minimum_length or len(code) > self.maximum_length:
+            return False
+        return sanitize(
+            self.gcc, self.clang, self.ccomp, code, f"-I {self.include_path}"
+        )
