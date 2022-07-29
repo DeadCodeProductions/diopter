843f2d4054a98fe51447107cd0188e4e4d725220
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2022-07-29 12:29:02 +0200

    wip

--- vdiff.py
+++ vdiff.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from multiprocessing import cpu_count
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
+from diopter.preprocessor import preprocess_csmith_code
 from diopter.reducer import Reducer
 from diopter.reduction_checks import make_interestingness_check
 from diopter.generator import CSmithGenerator
@@ -37,13 +38,6 @@ def get_value_ranges(
     return read_value_ranges(vrange_output)
 
 
-gcc11 = "/zdata/compiler_cache/gcc-releases-gcc-11.1.0/bin/gcc"
-gcc12 = "/zdata/compiler_cache/gcc-6f14b4e385d0b2221557faf40fb24eacf22ca7f8/bin/gcc"
-
-gcc11 = "/home/theo/gccs/gcc-releases-gcc-11.1.0/bin/gcc"
-gcc12 = "/home/theo/gccs/gcc-6f14b4e385d0b2221557faf40fb24eacf22ca7f8/bin/gcc"
-
-
 def is_range_interesting(a: Range, b: Range) -> bool:
     if a.inverse != b.inverse or a.type_ != b.type_:
         return True
@@ -66,47 +60,89 @@ def get_differing_value_ranges_from_instrumented(
     return diff_ranges
 
 
-def get_differing_value_ranges(code: str) -> tuple[str, str, list[tuple[str, Range, Range]]]:
+def get_differing_value_ranges(
+    code: str,
+) -> tuple[str, str, list[tuple[str, Range, Range]]]:
     # return [code, instrumented_code, list[tag,gcc_11_range,gcc_12_range]]
     instrumented_code = instrument(code)
-    return code, instrumented_code, get_differing_value_ranges_from_instrumented(
-        instrumented_code
+    return (
+        code,
+        instrumented_code,
+        get_differing_value_ranges_from_instrumented(instrumented_code),
     )
 
+
+def preprocess_and_get_differing_value_ranges(
+    code: str,
+) -> tuple[str, str, list[tuple[str, Range, Range]]]:
+    # return [code, instrumented_code, list[tag,gcc_11_range,gcc_12_range]]
+    code = preprocess_csmith_code(code, "gcc", ["-isystem/usr/include/csmith-2.3.0"])
+    instrumented_code = instrument(code)
+    return (
+        code,
+        instrumented_code,
+        get_differing_value_ranges_from_instrumented(instrumented_code),
+    )
+
+
 def reduction_check(code: str) -> bool:
-    return len(get_differing_value_ranges(code)) > 0
+    return len(get_differing_value_ranges(code)[2]) > 0
+
+
+gcc11 = "/zdata/compiler_cache/gcc-releases-gcc-11.1.0/bin/gcc"
+gcc12 = "/zdata/compiler_cache/gcc-6f14b4e385d0b2221557faf40fb24eacf22ca7f8/bin/gcc"
+
+gcc11 = "/home/theo/gccs/gcc-releases-gcc-11.1.0/bin/gcc"
+gcc12 = "/home/theo/gccs/gcc-6f14b4e385d0b2221557faf40fb24eacf22ca7f8/bin/gcc"
+
+gcc11 = "/home/theo/.cache/ccbuilder-compilers/gcc-01a9836c07053b3286b2259d6b628c7583413f55/bin/gcc"
+gcc12 = "gcc"
+
 
 if __name__ == "__main__":
+    interesting_cases = []
+    case_ = """
+    short a, b;
+    int c;
+    unsigned short(d)(unsigned e) { return e - a; }
+    void f() {
+      int g = 0;
+      for (;;) {
+        c = d(b);
+        g = d(g);
+      }
+    }
+    int main() {}
+    """
+    print(get_differing_value_ranges(case_)[1])
+    exit(0)
 
-    # interesting_cases = []
 
-    # with ProcessPoolExecutor(cpu_count()) as p:
-        # futures = []
-        # for case_ in CSmithGenerator().generate_code_parallel(100, p):
-            # futures.append(p.submit(get_differing_value_ranges, case_))
+    if False:
+        with ProcessPoolExecutor(cpu_count()) as p:
+            futures = []
+            for case_ in CSmithGenerator().generate_code_parallel(100, p):
+                futures.append(p.submit(preprocess_and_get_differing_value_ranges, case_))
 
-        # for future in as_completed(futures):
-            # code, instrumented_code, interesting_ranges = future.result()
-            # if interesting_ranges:
-                # with open("interesting_case", "w") as f:
-                    # f.write(code)
-                # exit(2)
-                # interesting_cases.append((instrumented_code, interesting_ranges))
-
+            for future in as_completed(futures):
+                code, instrumented_code, interesting_ranges = future.result()
+                if interesting_ranges:
+                    with open("interesting_case", "w") as f:
+                        f.write(code)
+                    exit(2)
+                interesting_cases.append((instrumented_code, interesting_ranges))
 
     with open("interesting_case", "r") as f:
-        instrumented_code = f.read()
+        code = f.read()
 
-
-
-
-    Reducer().reduce(
-        instrumented_code,
+    reduced = Reducer().reduce(
+        code,
         make_interestingness_check(
             reduction_check, True, "-I/usr/include/csmith-2.3.0", {}
         ),
     )
 
+    print(get_differing_value_ranges(reduced)[2])
 
     # for instrumented_code, _ in interesting_cases:
     # Reducer().reduce(
@@ -115,7 +151,6 @@ if __name__ == "__main__":
     # reduction_check, True, "-I/usr/include/csmith-2.3.0", {}
     # ),
     # )
-
 
     # while True:
     # print("Trying new case")
